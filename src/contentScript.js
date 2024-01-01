2161a96c5626ee79e3bd75ec0cadc70987f6d88f
Dinh Hai <67992547+dinhhai1205@users.noreply.github.com>
2024-01-02 00:19:06 +0700

    Update contentScript.js

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,16 +1,3 @@
-// // --------------------------------------------------------------------------------
-// chrome.runtime.sendMessage(
-//   {
-//     type: 'SUBMIT',
-//     payload: {
-//       message: 'Hello, my name is Con. I am from ContentScript.',
-//     },
-//   },
-//   (response) => {
-//     console.log(response.message);
-//   }
-// );
-console.log('Content scripts')
 // Listen for message
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log('From content script')
